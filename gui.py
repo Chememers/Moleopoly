1495c5df3b0501adb829654022f8fe01606b855f
Aditya Matam <aditya0310@outlook.com>
2020-10-22 18:03:41 -0700

    Const file

--- gui.py
+++ gui.py
@@ -1,39 +1,56 @@
 from tkinter import Tk, Canvas
-from moleopoly import Board
+from tkinter.constants import BOTH
+from moleopoly import Board, ElementSquare
+from const import CENTERSIZE, SIZE, SQSHORT, SQLONG
 
 
-class Game(Board):
-    def __init__(self, players: list):
+class ElementSquareGUI:
+    def __init__(self, canvas, square, side):
+        self.canvas = canvas
+        self.square = square
+        self.side = side
+
+
+class GUI(Board):
+    def __init__(self, master, players: list):
         super().__init__(*players)
+        self.win = master
+        self.setup_board()
+
+    def setup_board(self):
+        self.canvas = Canvas(self.win, bg="white", width=SIZE, height=SIZE)
+        self.canvas.pack(fill=BOTH)
+        self.canvas.create_rectangle(
+            SQLONG, SQLONG, SQLONG + CENTERSIZE, SQLONG + CENTERSIZE, fill="#74BBFB"
+        )
+
+        # dirs = ("W", "N", "E", "S")
+        # ranges = ((1, 8), (9, 16), (17, 24), (25, 32))
+        # for i in range(4):
+        #     for j in range(*ranges[i]):
+        #         sq = self.squares[j]
+        #         if isinstance(sq, ElementSquare):
+        #             ElementSquareGUI(self.canvas, sq, dirs[i])
 
 
-def get_players():
-    players = []
-    print("=" * 10)
-    num = int(input("Number of Players: "))
-    for i in range(num):
-        players.append(input(f"Name {i+1}: "))
-    print("=" * 10)
-    print("Starting game with: ", end="")
-    print(*players, sep=", ", end="\n")
-    print("=" * 10)
-    return players
+players = ("Aditya", "Gowtham", "Hari")
+# players = get_players()
 
+win = Tk()
+game = GUI(win, players)
+win.update_idletasks()
+win.resizable(False, False)
+win.mainloop()
 
-game = Game(get_players())
+# def get_players():
+# players = []
+# print("=" * 10)
+# num = int(input("Number of Players: "))
+# for i in range(num):
+#     players.append(input(f"Name {i+1}: "))
+# print("=" * 10)
+# print("Starting game with: ", end="")
+# print(*players, sep=", ", end="\n")
+# print("=" * 10)
+# return players
 
-# class GUI(Canvas):
-#     WIDTH = 800
-#     HEIGHT = 800
-
-#     def __init__(self, root):
-#         super().__init__(master=root, width=GUI.WIDTH, height=GUI.HEIGHT, bg="white")
-#         self.root = root
-#         self.pack()
-
-
-# win = Tk()
-# GUI(win)
-# win.resizable(False, False)
-# win.update_idletasks()
-# win.mainloop()
