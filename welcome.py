63874b11a38b62aa2d0f5a34e5480ca6a8295922
harirakul <harirakul.a@gmail.com>
2020-10-23 10:19:35 -0700

    Added players

--- welcome.py
+++ welcome.py
@@ -2,10 +2,12 @@ import PySimpleGUI as sg
 
 sg.theme('DarkGreen1')   
 
+players = []
+
 layout = [  [sg.Text('New Game', font=('Comic Sans MS', 20))],
-            [sg.InputText(), sg.Button('Add Player')],
+            [sg.InputText(key="Name"), sg.Button('Add Player')],
             [sg.Text('Players:', font=('Comic Sans MS', 20))],
-            [sg.Listbox(values=[], size=(30, 6))],
+            [sg.Listbox(values=players, key="players", size=(30, 6))],
             [sg.Button('Play!'), sg.Button('Cancel')] ]
 
 window = sg.Window('Mole-O-Poly', layout, element_justification="c")
@@ -14,6 +16,10 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    if event in ("Add Player"):
+        players.append(values["Name"])
+        window.Element('players').Update(values=players)
+
+    #print('You entered ', values[0])
 
 window.close()
